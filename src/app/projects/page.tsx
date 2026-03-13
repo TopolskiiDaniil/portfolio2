786e246da27c57b080f6d09bac0afe540dcef43a
TopolskiiDaniil <cool.topolskiy@gmail.com>
2026-03-13 19:21:39 +0300

    Add description for projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -9,22 +9,43 @@ export default function ProjectsPage() {
 
         <section className={styles.projects__container}>
           <h2 className={styles.projects__title}>Мои последние работы.</h2>
-          <ul className={styles.project__list}>
+          <ul className={styles.projects__list}>
 
             <li className={styles.projects__item}>
               <article className={styles.projects__card}>
                 <header className={styles.project__header}>
                   <h3 className={styles.project__title}>Личный проект «Шесть городов»</h3>
                 </header>
-                <Image
-                  className={styles.project__image}
-                  src='/images/projects/six-cities.jpg'
-                  width={465}
-                  height={219}
-                  alt="проект «Шесть городов»" />
-                <p className={styles.project__description}>
-                  Успешно выполненный проект в рамках интенсива «JavaScript. Разработка сложных клиентских приложений».
-                </p>
+                <figure className={styles.project__figure}>
+                  <Image
+                    className={styles.project__image}
+                    src='/images/projects/six-cities.jpg'
+                    width={465}
+                    height={219}
+                    alt="проект «Шесть городов»" />
+                </figure>
+                <div className={styles.project__description}>
+                  <p className={styles.project__appointment}>Веб-приложение для размещения и поиска объявлений аренды жилья с интерактивной картой и фильтрацией.</p>
+                  <h4 className={styles[`project__title-list`]}>Функциональность:</h4>
+                  <ul className={styles.project__list}>
+                    <li>интерактивная карта объявлений</li>
+                    <li>фильтрация по городам</li>
+                    <li>сортировка по цене и рейтингу</li>
+                    <li>форма авторизации</li>
+                    <li>создание отзывов и их валидация</li>
+                    <li>работа с "избранными" объявлениями</li>
+                  </ul>
+                  <h4 className={styles[`project__title-list`]}>Технологии:</h4>
+                  <ul className={styles.project__list}>
+                    <li>React/TS</li>
+                    <li>Redux/RTK</li>
+                    <li>Leaflet</li>
+                    <li>Axios</li>
+                    <li>Vitest</li>
+                    <li>React-testing-library</li>
+                    <li>Vite</li>
+                  </ul>
+                </div>
                 <footer className={styles.project__footer}>
                   <Link
                     className={styles.project__button}
@@ -36,8 +57,8 @@ export default function ProjectsPage() {
                   <Link
                     className={styles.project__button}
                     href='https://586815-six-cities-3.vercel.app/'
-                    type="игеещт"
-                    target="_blank">
+                    target="_blank"
+                    rel="noopener noreferrer">
                     Демо
                   </Link>
                 </footer>
@@ -49,21 +70,38 @@ export default function ProjectsPage() {
                 <header className={styles.project__header}>
                   <h3 className={styles.project__title}>Личный проект «Большое путешествие»</h3>
                 </header>
-                <Image
-                  className={styles.project__image}
-                  src='/images/projects/big-trip.webp'
-                  width={465}
-                  height={219}
-                  alt="проект «Большое путешествие»" />
-                <p className={styles.project__description}>
-                  Успешно выполненный проект в рамках интенсива «JavaScript. Архитектура клиентских приложений».
-                </p>
+                <figure className={styles.project__figure}>
+                  <Image
+                    className={styles.project__image}
+                    src='/images/projects/big-trip.webp'
+                    width={465}
+                    height={219}
+                    alt="Интерфейс приложения Big Trip — планировщик путешествий" />
+                </figure>
+                <div className={styles.project__description}>
+                  <p className={styles.project__appointment}>Big Trip — планировщик путешествий. На данном проекте разбирал принципы ООП и строил самописную flux-архитектуру.</p>
+                  <h4 className={styles[`project__title-list`]}>Функциональность:</h4>
+                  <ul className={styles.project__list}>
+                    <li>управление маршрутами</li>
+                    <li>добавление и редактирование событий</li>
+                    <li>фильтрация поездок</li>
+                    <li>синхронизация с API</li>
+                  </ul>
+                  <h4 className={styles[`project__title-list`]}>Технологии:</h4>
+                  <ul className={styles.project__list}>
+                    <li>JavaScript</li>
+                    <li>MVP архитектура</li>
+                    <li>REST API</li>
+                    <li>Webpack</li>
+                    <li>date libraries</li>
+                  </ul>
+                </div>
                 <footer className={styles.project__footer}>
                   <Link
                     className={styles.project__button}
                     href='https://github.com/TopolskiiDaniil/586815-big-trip-2'
-                    type="button"
-                    target="_blank">
+                    target="_blank"
+                    rel="noopener noreferrer">
                     Код проекта
                   </Link>
                 </footer>
@@ -75,21 +113,35 @@ export default function ProjectsPage() {
                 <header className={styles.project__header}>
                   <h3 className={styles.project__title}>Личный проект «Кекстаграм»</h3>
                 </header>
-                <Image
-                  className={styles.project__image}
-                  src='/images/projects/keksogram.jpg'
-                  width={400}
-                  height={300}
-                  alt="проект «Кекстаграм»" />
-                <p className={styles.project__description}>
-                  Успешно выполненный проект в рамках интенсива «JavaScript. Профессиональная разработка веб-интерфейсов».
-                </p>
+                <figure className={styles.project__figure}>
+                  <Image
+                    className={styles.project__image}
+                    src='/images/projects/keksogram.jpg'
+                    width={400}
+                    height={300}
+                    alt="проект «Кекстаграм»" />
+                </figure>
+                <div className={styles.project__description}>
+                  <p className={styles.project__appointment}>Веб-приложение — галерея изображений</p>
+                  <h4 className={styles[`project__title-list`]}>Функциональность:</h4>
+                  <ul className={styles.project__list}>
+                    <li>загрузка фото</li>
+                    <li>просмотр в модальном окне</li>
+                    <li>комментарии</li>
+                    <li>фильтрация изображений</li>
+                  </ul>
+                  <h4 className={styles[`project__title-list`]}>Технологии:</h4>
+                  <ul className={styles.project__list}>
+                    <li>JavaScript (ES6+)</li>
+                    <li>Vite</li>
+                  </ul>
+                </div>
                 <footer className={styles.project__footer}>
                   <Link
                     className={styles.project__button}
                     href='https://github.com/TopolskiiDaniil/586815-kekstagram-33'
-                    type="button"
-                    target="_blank">
+                    target="_blank"
+                    rel="noopener noreferrer">
                     Код проекта
                   </Link>
                 </footer>
@@ -101,28 +153,44 @@ export default function ProjectsPage() {
                 <header className={styles.project__header}>
                   <h3 className={styles.project__title}>Личный проект «Кэт энерджи»</h3>
                 </header>
-                <Image
-                  className={styles.project__image}
-                  src='/images/projects/cat-energy.jpg'
-                  width={465}
-                  height={219}
-                  alt="проект «Кэт энерджи»" />
-                <p className={styles.project__description}>
-                  Успешно выполненный проект в рамках интенсива «HTML и CSS. Адаптивная вёрстка и автоматизация».
-                </p>
+                <figure className={styles.project__figure}>
+                  <Image
+                    className={styles.project__image}
+                    src='/images/projects/cat-energy.jpg'
+                    width={465}
+                    height={219}
+                    alt="проект «Кэт энерджи»" />
+                </figure>
+                <div className={styles.project__description}>
+                  <p className={styles.project__appointment}>Cайт фитнес-питания для котов 🐱</p>
+                  <h4 className={styles[`project__title-list`]}>Функциональность:</h4>
+                  <ul className={styles.project__list}>
+                    <li>адаптивная верстка (mobile-first)</li>
+                    <li>интерактивный слайдер сравнения</li>
+                    <li>оптимизированная графика</li>
+                  </ul>
+                  <h4 className={styles[`project__title-list`]}>Технологии:</h4>
+                  <ul className={styles.project__list}>
+                    <li>HTML5</li>
+                    <li>SCSS</li>
+                    <li>BEM</li>
+                    <li>JavaScript (ES6+)</li>
+                    <li>Gulp</li>
+                  </ul>
+                </div>
                 <footer className={styles.project__footer}>
                   <Link
                     className={styles.project__button}
                     href='https://github.com/TopolskiiDaniil/586815-cat-energy-32'
-                    type="button"
-                    target="_blank">
+                    target="_blank"
+                    rel="noopener noreferrer">
                     Код проекта
                   </Link>
                   <Link
                     className={styles.project__button}
                     href='https://topolskiidaniil.github.io/586815-cat-energy-32/'
-                    type="игеещт"
-                    target="_blank">
+                    target="_blank"
+                    rel="noopener noreferrer">
                     Демо
                   </Link>
                 </footer>
@@ -134,28 +202,40 @@ export default function ProjectsPage() {
                 <header className={styles.project__header}>
                   <h3 className={styles.project__title}>Личный проект «Седона»</h3>
                 </header>
-                <Image
-                  className={styles.project__image}
-                  src='/images/projects/sedona.jpg'
-                  width={400}
-                  height={300}
-                  alt="проект «Седона»" />
-                <p className={styles.project__description}>
-                  Успешно выполненный проект в рамках интенсива «HTML и CSS. Профессиональная вёрстка сайтов».
-                </p>
+                <figure className={styles.project__figure}>
+                  <Image
+                    className={styles.project__image}
+                    src='/images/projects/sedona.jpg'
+                    width={400}
+                    height={300}
+                    alt="проект «Седона»" />
+                </figure>
+                <div className={styles.project__description}>
+                  <p className={styles.project__appointment}>Многостраничный адаптивный сайт с каталогом гостиниц.</p>
+                  <h4 className={styles[`project__title-list`]}>Функциональность:</h4>
+                  <ul className={styles.project__list}>
+                    <li>семантичная верстка</li>
+                    <li>сайд-эффекты</li>
+                  </ul>
+                  <h4 className={styles[`project__title-list`]}>Технологии:</h4>
+                  <ul className={styles.project__list}>
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                  </ul>
+                </div>
                 <footer className={styles.project__footer}>
                   <Link
                     className={styles.project__button}
                     href='https://github.com/TopolskiiDaniil/586815-sedona-40'
-                    type="button"
-                    target="_blank">
+                    target="_blank"
+                    rel="noopener noreferrer">
                     Код проекта
                   </Link>
                   <Link
                     className={styles.project__button}
                     href='https://topolskiidaniil.github.io/586815-sedona-40/'
-                    type="игеещт"
-                    target="_blank">
+                    target="_blank"
+                    rel="noopener noreferrer">
                     Демо
                   </Link>
                 </footer>
